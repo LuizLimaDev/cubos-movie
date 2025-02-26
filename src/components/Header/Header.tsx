810e329de301944dd083f232c2efad06437ae13b
LuizLimaDev <devluizlima@gmail.com>
2025-02-25 21:11:43 -0300

    feat: logo navigate to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import sun from "@/assets/icons/sun.svg";
 import logo from "@/assets/logo-cubos.svg";
+import { Link } from "react-router";
 import CmSmallButton from "../ui/CmSmallButton/CmSmallButton";
 
 export default function Header() {
   return (
     <header className="flex h-20 w-full items-center justify-between border-b border-mauveDark-7 p-4">
       <div className="flex h-[36px] w-[247px] items-center justify-center gap-4">
-        <img src={logo} alt="cubos movie logo" width={160} height={36} />
+        <Link to="/">
+          <img src={logo} alt="cubos movie logo" width={160} height={36} />
+        </Link>{" "}
         <h1 className="text-bold mt-1 font-inter text-xl text-mauve-1">
           Movies
         </h1>
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,8 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzI5YWY0Nzk2NjhmN2U2MTAxZTQ0NzYwZmFkZmJiYSIsIm5iZiI6MTc0MDE4NzE4Ny40OTgsInN1YiI6IjY3YjkyNjMzMmVkYzljMTNmYjBhNjRlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jFsc7nrUmJSmrrbr_rOclQvAx8iY2GvYwkEonpUnA5s",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
     accept: "application/json",
   },
 });
