b21819ab0292328f70c2638b97b1140f7816b5c4
LuizLimaDev <devluizlima@gmail.com>
2025-02-25 21:10:39 -0300

    style: fix text style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
   return (
     <footer className="h-20 w-full flex-col items-center justify-center border-t border-mauveDark-7 pt-5">
-      <p className="text-center font-montserrat text-base font-bold text-mauveDark-11">
-        2023 © Todos os direitos reservados a Cubos Movies
+      <p className="text-center font-montserrat text-base text-mauveDark-11">
+        2023 © Todos os direitos reservados a{" "}
+        <span className="font-bold">Cubos Movies</span>
       </p>
     </footer>
   );
